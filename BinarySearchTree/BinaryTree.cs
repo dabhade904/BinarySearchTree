5c3ecd706621cf21997895fc85070b0465c920d0
dabhade904 <dabhade904@gmail.com>
2022-08-14 15:33:05 +0530

    Added : Binary search tree Project

---
+++ BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinaryTree
+    {
+        public Node Root { get;  set; }
+        public bool Add(int value)
+        {
+            Node before= null,after= this.Root;
+            while(after != null)
+            {
+                before = after;
+                if (value < after.data)
+                {
+                    after = after.LeftNode;
+                }else if(value > after.data)
+                {
+                    after= after.RightNode;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            Node newNode = new Node();
+            newNode.data = value;
+            if (this.Root == null)
+            {
+                this.Root = newNode;
+            }
+            else
+            {
+                if (value < before.data)
+                {
+                    before.LeftNode = newNode;
+                }
+                else
+                {
+                    before.RightNode = newNode; 
+                }
+            }
+            return true;
+        }
+
+        public void TraversePreorder(Node parent)
+        {
+            if(parent!= null)
+            {
+                Console.WriteLine(parent.data + " ");
+                TraversePreorder(parent.LeftNode);
+                TraversePreorder(parent.RightNode);
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BinarySearchTree
+{
+    public class Program
+    {
+        public static void Main(string [] args)
+        {
+            BinaryTree binaryTree = new BinaryTree();
+            binaryTree.Add(50);
+            binaryTree.Add(17);
+            binaryTree.Add(72);
+            binaryTree.Add(23);
+            binaryTree.Add(76); 
+            binaryTree.Add(54);
+            binaryTree.Add(12);
+            binaryTree.Add(14);
+            binaryTree.Add(9);
+            binaryTree.Add(67);
+            binaryTree.Add(19);
+            binaryTree.TraversePreorder(binaryTree.Root);
+        }
+    }
+}
